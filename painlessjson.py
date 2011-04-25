df1294339e034eaea34f5683ad9de1cf26aaf7a8
Jeremy Fleischman <jeremyfleischman@gmail.com>
2011-04-24 19:33:00 -0700

    Cgi script mode working =)

--- painlessjson.py
+++ painlessjson.py
@@ -2,7 +2,7 @@
 
 # TODO - it would be nice if domains were somehow protected
 # from one another... i think that's impossible, however
-import anydbm
+import anydbm, os
 
 def key(user, domain):
     return '%s@%s' % ( user, domain )
@@ -17,37 +17,44 @@ def get(user, domain):
 try:
     db = anydbm.open('json', 'c')
     if __name__ == "__main__":
-        import sys
-        if len(sys.argv) == 3:
-            _, user, domain = sys.argv
-            print(get(user, domain))
-        elif len(sys.argv) == 4:
-            _, user, domain, val = sys.argv
-            put(user, domain, val)
-        else:
-            print('Usage: %s user domain val' % sys.argv[0])
-            sys.exit(1)
-    else:
-        import cgi, cgitb, json
-        cgitb.enable()
+        if 'QUERY_STRING' in os.environ:
+            import cgi, cgitb
+            try:
+                import json
+            except ImportError:
+                import simplejson as json
+            cgitb.enable()
 
-        # TODO - error checking
-        form = cgi.FieldStorage()
-        user = form.getfirst('user')
-        domain = form.getfirst('domain')
-        val = form.getfirst('val')
-        callback = form.getfirst('callback')
+            # TODO - error checking
+            form = cgi.FieldStorage()
+            user = form.getfirst('user')
+            domain = form.getfirst('domain')
+            val = form.getfirst('val')
+            callback = form.getfirst('callback')
 
-        result = {}
-        if val is None:
-            val = get(user, domain)
-            result['val'] = val
+            result = {}
+            result['success'] = False
+            if user != None and domain != None:
+                if val is None:
+                    val = get(user, domain)
+                    result['val'] = val
+                    result['success'] = True
+                else:
+                    put(user, domain, val)
+                    result['success'] = True
+            print "Content-type: application/json\r\n\r\n",
+            print(json.dumps(result))
         else:
-            put(user, domain, val)
-        result['success'] = True
-        print("Content-type: application/json")
-        print()
-        print(json.dumps(result))
+            import sys
+            if len(sys.argv) == 3:
+                _, user, domain = sys.argv
+                print(get(user, domain))
+            elif len(sys.argv) == 4:
+                _, user, domain, val = sys.argv
+                put(user, domain, val)
+            else:
+                print('Usage: %s user domain val' % sys.argv[0])
+                sys.exit(1)
 
 finally:
     db.close()
